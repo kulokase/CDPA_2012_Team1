fc9a668dfe94924a59f3e6c6056b96e09ad75d4d
PichuChen <Pichubaby@gmail.com>
2012-07-27 02:51:37 +0800

    用class來設定style

--- dormnet119/index.php
+++ dormnet119/index.php
@@ -3,13 +3,24 @@
 	<head>
 		<meta charset = "utf-8">
 		<title>宿網報修首頁</title>
+		<style>
+			input.btn{
+				border: 0px solid #BB9EF8;
+				color :#333333;
+				height:45px;
+				width: 160px;
+				background:  url(IMG/index/cat.gif);
+				font-size: 26px;
+				color: green;	
+			}
+		</style>
 	</head>
 	<body style="background: url(IMG/index/k18.gif)" >
 		<center>
 		<br/>
-                <input name="submit"   type=submit  style="border: 0px solid #BB9EF8;color :#333333;height:45px; width: 160px; background:  url(IMG/index/cat.gif); font-size: 26px; color: green; " value='報修點我' onclick="window.open('repair/p1.php');"/>
-        	<input name="submit"   type=submit  style="border: 0px solid #BB9EF8;color :#333333;height:45px; width: 160px; background:  url(IMG/index/cat.gif); font-size: 26px ;color: green" value='進度查詢' onclick="window.open('search.php');"/>
-        	<input name="submit" type=submit style="border: 0px solid #BB9EF8;color :#333333;height:45px; width: 160px; background:  url(IMG/index/cat.gif); font-size: 26px; color: green" value='教學點我' onclick="window.open('teach.html');">
+                <input name="submit" type=submit class="btn" value='報修點我' onclick="window.open('repair/p1.php');"/>
+        	<input name="submit" type=submit class="btn" value='進度查詢' onclick="window.open('search.php');"/>
+        	<input name="submit" type=submit class="btn" value='教學點我' onclick="window.open('teach.html');">
 		<br/><br/><br/>
 		<span style= "color:red"><font size = "5"><strong>流量Top100!!</strong></font></span>
 		<br/><br/>
