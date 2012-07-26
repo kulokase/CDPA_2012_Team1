04ac548dc5f438591512ca3728a8b5b9ecbeee3e
wonderfu <b123e321n@gmail.com>
2012-07-26 19:16:48 +0800

    排版 p1.php

--- dormnet119/repair/p1.php
+++ dormnet119/repair/p1.php
@@ -1,17 +1,14 @@
-<htmarset = "utf8">
-<head><meta charset = "utf8">
-      <title> 報修單 </title>
-</head>
-<body>
+<html>
+	<head>
+		<meta charset = "utf8">
+		<title> 報修單 </title>
+	</head>
+	<body>
+		<h1><center>中山大學宿網報修</center></h1>
 
-<h1><center>中山大學宿網報修</center></h1>
-
-<BUTTON style="WIDTH:150px ; HEIGHT:75px" onclick="self.location.href='p2.php'">填報修單</BUTTON>
-<BUTTON style="WIDTH:150px ; HEIGHT:75px" onclick="self.location.href='p4.php'">改報修單</BUTTON>
-<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-<BUTTON style="WIDTH:60px ; HEIGHT:30px" onclick="self.location.href='../index.php'" >回首頁</BUTTON>
-
-
-
-</body>
+		<center><BUTTON style="WIDTH:150px ; HEIGHT:75px" onclick="self.location.href='p2.php'">填報修單</BUTTON>
+		<BUTTON style="WIDTH:150px ; HEIGHT:75px" onclick="self.location.href='p4.php'">改報修單</BUTTON>
+		<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+		<BUTTON style="WIDTH:60px ; HEIGHT:30px" onclick="self.location.href='../index.php'" >回首頁</BUTTON></center>
+	</body>
 </html>
