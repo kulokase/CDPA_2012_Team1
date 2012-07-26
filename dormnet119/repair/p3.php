f9b3f257f7bcad46ed4292cfc07bf672a3b7626f
YS_Wu <bmwv12lmr@gmail.com>
2012-07-26 14:42:28 +0800

    good

--- dormnet119/repair/p3.php
+++ dormnet119/repair/p3.php
@@ -2,7 +2,7 @@
 	<head>
 		<script type="text/javascript">
 		function suc() {
-			alert("您已報修成功"<br/>"以下為查詢碼"<br/>$c<br/>"請妥善保管!!!"<br/>"另以寄送此查詢碼至您信箱")
+			alert("您已報修成功"<br/>"以下為查詢碼"<br/>$c<br/>"請妥善保管!!!"<br/>"已寄送此查詢碼至您的信箱")
 		}
 		</script>
 		<meta charset="utf-8">
@@ -11,7 +11,29 @@
 			姓名:<input type="text" name="n"><br/>
 			手機:<input type="text" name="p"><br/>
 			E-mail:<input type="text" name="e"><br/>
-			寢室床位:武嶺/翠亨<input type="text" name="d">村/棟<input type="type" name="r">室<input type="type" nmae="b">床<br/>
+			寢室床位:<select name="d">
+ 				<option value="" selected="selected">&lt;--請選擇--&gt;</option>
+  				<option value="1">武嶺一村</option>
+				<option value="2">武嶺二村</option>
+ 				<option value="3">武嶺三村</option>
+  				<option value="4">武嶺四村</option>
+  				<option value="A">翠亨A棟</option>
+  				<option value="B">翠亨B棟</option>
+  				<option value="C">翠亨C棟</option>
+  				<option value="D">翠亨D棟</option>
+  				<option value="E">翠亨E棟</option>
+  				<option value="F">翠亨F棟</option>
+  				<option value="G">翠亨G棟</option>
+  				<option value="H">翠亨H棟</option>
+  				<option value="L">翠亨L棟</option>
+  				</select><input type="type" name="r">室  
+				<select name="b">
+  				<option value="" selected="selected">&lt;--請選擇--&gt;</option>
+ 				<option value="1">1</option>
+  				<option value="2">2</option>
+  				<option value="3">3</option>
+  				<option value="4">4</option>
+  				</select>床<br/>
 			維修時段:<select name="w">
   					<option value="" selected="selected">&lt;--請選擇--&gt;</option>
  					<option value="1">星期一</option>
