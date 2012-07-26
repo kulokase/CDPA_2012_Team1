41e2b55726b6a0c92d504f5d4d1a5db1a15d3030
wonderfu <b123e321n@gmail.com>
2012-07-26 19:12:51 +0800

    update p2.php

--- dormnet119/repair/p2.php
+++ dormnet119/repair/p2.php
@@ -1,5 +1,4 @@
 <html>
-
 <head>
          <meta charset = "utf8"> 
          <title> 前置檢查 </title>
@@ -7,42 +6,38 @@
       	 h2 {color:red;}
          </style>
 </head>
-
 <body>
+	<h1><center>前置檢查</center></h1>
+	<br/><br/>
 
-<h1><center>前置檢查</center></h1>
-<br/><br/>
+	<h2><center>如因超流或其他違規行為被鎖網，網推會將不提供報修服務!</center></h2>
+	<br/><br/>
+	<marquee>WTF 我會移動!!!!！</marquee>
 
-<h2><center>如因超流或其他違規行為被鎖網，網推會將不提供報修服務!</center></h2>
-<br/><br/>
-<marquee>WTF 我會移動!!!!！</marquee>
+	<center><table>
+		<tr>
+			<th><center>問題：</center></th>
+			<th><center>Y</center></th>
+			<th><center>N</center></th>
+		</tr>
+		<tr>
+			<form  method="get">
+				<td>將自己的電腦連接同房的網孔後是否能連線: </td>
+				<td><input type = "radio" name="check1" id="y" /></td>
+				<td><input type = "radio" name="check1" id="n"/><br/></td>
+			</form>
+		</tr>
+		<tr>
+			<form method="get">
+                	        <td>將自己的電腦連接同房的網孔後是否能連線: </td>
+                       		<td><input type = "radio" name="check2" id="y"  /></td>
+                       		<td><input type = "radio" name="check2" id="n"/><br/></center></td>
+          	      </form>
+		</tr>
+	</table></center>
 
-<center><table>
-	<tr>
-		<th><center>問題：</center></th>
-		<th><center>Y</center></th>
-		<th><center>N</center></th>
-	</tr>
-	<tr>
-		<form  method="get">
-			<td>將自己的電腦連接同房的網孔後是否能連線: </td>
-			<td><input type = "radio" name="check1" id="y" /></td>
-			<td><input type = "radio" name="check1" id="n"/><br/></td>
-		</form>
-	</tr>
-	<tr>
-		<form method="get">
-                        <td>將自己的電腦連接同房的網孔後是否能連線: </td>
-                        <td><input type = "radio" name="check2" id="y"  /></td>
-                        <td><input type = "radio" name="check2" id="n"/><br/></center></td>
-                </form>
-	</tr>
-</table></center>
+	<br/><br/><br/>
 
-<br/><br/><br/>
-
-<center><input type="submit" value="送出"  onclick="self.location.href='p3.php'"/></center>
-
-</body>
-
+	<center><input type="submit" value="送出"  onclick="self.location.href='p3.php'"/></center>
+	</body>
 </html>
