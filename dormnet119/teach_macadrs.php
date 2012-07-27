e7046ed9131ead85dafbf2a362e1a65ff2af7bde
kansokusha <kansokusha@gmail.com>
2012-07-27 09:45:39 +0800

    merge commit

---
+++ dormnet119/teach_macadrs.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+
+	<head>
+		<meta charset = "unif-8">
+
+		DORMNET-119
+
+	</head>
+<body>
+	<li> <a href="#XP">Windows XP版本</a>  <li><a href="#WIN7">Windows 7</a>
+	<hr />
+
+	<div class ="content_xp">
+	<h2>網路卡號查詢</h2>
+	<ul>
+		<li> <a name="XP">Windows XP </a> </li>
+
+	<ol>
+
+		<li>開始->執行</li>
+		<img alt="teach_xp_mac1" title="teach_xp_mac1" src ="http://140.117.202.135/~kansokusha/CDPA_2012_Team1/dormnet119/IMG/teach/XP01.JPG">
+		<li>在執行的欄位輸入 "cmd" </li>
+		<img alt="teach_xp_mac2" title="teach_xp_mac2" src ="http://140.117.202.135/~kansokusha/CDPA_2012_Team1/dormnet119/IMG/teach/XP02.JPG">
+		<li>在黑色的框框中輸入 "ipconfig /all"  Physical Address 即為你的網路卡卡號(MAC Address)</li>
+		<img alt="teach_xp_mac3" title="teach_xp_mac3" src ="http://140.117.202.135/~kansokusha/CDPA_2012_Team1/dormnet119/IMG/teach/XP03.JPG">
+
+	</ol>
+	
+		<li> <a name = "WIN7">Windows 7 </a> </li>
+
+
+	<ol>
+		
+
+
+
+	</ol>
+
+	</div>
+</body>
+
+</html>
